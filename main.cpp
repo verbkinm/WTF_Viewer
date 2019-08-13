cefd0effb8290e30677cb88f0c7f05e3d626365a
verbkinm <verbkinm@yandex.ru>
2019-08-13 15:47:56 +0500

    0.9.8.6 1. Уменьшено потребление оперативной памяти из ~ 75 Мб до ~20 Мб при запуске программы за счет уменьшения картинок в файле ресурсов

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 
 #include "mainwindow.h"
 
-//#include "viewer/viewer.h"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-//    Viewer w;
 //    w.show();
 
-//    for (auto &child : w.children()) {
-//        qDebug() << sizeof (child);
-//    }
     w.showMaximized();
 
     return a.exec();
